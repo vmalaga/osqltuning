13ca1476994998c28fe767a66c2f10b66194c1e1
Vicente Málaga <vmalaga@gmail.com>
2013-07-23 18:21:30 +0200

    add dbconnections model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DbConnections(models.Model):
+	conname = models.CharField("Connection Name", max_length=40)
+	hostname = models.CharField("Hostname or IP", max_length=100)
+	username = models.CharField("Username to connect", max_length=30)
+	password = models.CharField(max_length=100)
+	servicename = models.CharField("Oracle ServiceName", max_length=100)
+
+	def __unicode(self):
+		return self.name
